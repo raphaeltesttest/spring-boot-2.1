b4764b18d62b4ea6eacbad2b784a9fa7e79debdf
marianos <marianos8125@gmail.com>
2021-01-30 09:17:22 -0300

    connect Postgres db - flyway, jdbc template and property files

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,14 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +26,34 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+
+        final String sql = "SELECT id, name FROM person";
+        List<Person> people = jdbcTemplate.query(sql, ((resultSet, i) -> {
+            return new Person(
+                    UUID.fromString(resultSet.getString("id")),
+                    resultSet.getString("name")
+            );
+        }));
+
+        return people;
+
+//        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, ((resultSet, i) -> {
+            return new Person(
+                    UUID.fromString(resultSet.getString("id")),
+                    resultSet.getString("name")
+            );
+        }));
+
+        return Optional.ofNullable(person);
+
+//        return Optional.empty();
     }
 
     @Override
